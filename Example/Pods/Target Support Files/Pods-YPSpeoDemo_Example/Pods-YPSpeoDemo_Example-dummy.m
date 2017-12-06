802cbaced79dd65e9d88bb6c3ece745f89af102a
yaopeng <yaop@dinglc.com>
2017-12-06 15:56:30 +0800

    Initial Commit of Library
    Signed-off-by: yaopeng <yaop@dinglc.com>

---
+++ Example/Pods/Target Support Files/Pods-YPSpeoDemo_Example/Pods-YPSpeoDemo_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YPSpeoDemo_Example : NSObject
+@end
+@implementation PodsDummy_Pods_YPSpeoDemo_Example
+@end
---
+++ Example/Pods/Target Support Files/YPSpeoDemo/YPSpeoDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YPSpeoDemo : NSObject
+@end
+@implementation PodsDummy_YPSpeoDemo
+@end
